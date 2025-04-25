bd7294d460cfbd0ce205fc398a0c3b16a039f8be
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-04-25 13:48:42 +0300

    final solution

--- solutions/Theme3/TaskI.cpp
+++ solutions/Theme3/TaskI.cpp
@@ -1,79 +1,115 @@
 #include <iostream>
+#include <cmath>
+#include <utility>
+#include <cmath>
+#include <limits>
+#include <iomanip>
 #include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
 #include <string>
 
-class StringHasher {
-private:
-    const long long prime = 137;
-    const long long mod = 1e9 + 7;
-    std::vector<long long> power;
-    std::vector<long long> hashes;
-    size_t string_length;
+#define ll long long
 
-public:
-    StringHasher(const std::string& s) : string_length(s.size()) {
-        computePowerArray();
-        computeHashArray(s);
+using namespace std;
+
+ll calculateGcd(ll firstNumber, ll secondNumber) {
+    if (secondNumber == 0)
+        return firstNumber;
+    return calculateGcd(secondNumber, firstNumber % secondNumber);
+}
+
+ll calculateFastPower(ll baseNumber, ll exponent, ll modulus) {
+    if (exponent == 0)
+        return 1;
+    if (exponent % 2 == 0) {
+        ll temp = calculateFastPower(baseNumber, exponent / 2, modulus) % modulus;
+        return (temp * temp) % modulus;
     }
+    else {
+        return ((baseNumber % modulus) * calculateFastPower(baseNumber, exponent - 1, modulus) % modulus) % modulus;
+    }
+}
 
-    void computePowerArray() {
-        power.resize(string_length);
-        power[0] = 1;
-        for (size_t i = 1; i < string_length; ++i) {
-            power[i] = (power[i - 1] * prime) % mod;
+void calculatePrimeFactors(ll number, map<ll,ll>& primeFactors) {
+    for (int divisor = 2; divisor * divisor <= number; ++divisor) {
+        while (number % divisor == 0) {
+            primeFactors[divisor]++;
+            number /= divisor;
         }
     }
+    if (number > 1)
+        primeFactors[number]++;
+}
 
-    void computeHashArray(const std::string& s) {
-        hashes.resize(string_length + 1);
-        hashes[0] = 0;
-        for (size_t i = 1; i <= string_length; ++i) {
-            hashes[i] = (hashes[i - 1] + (s[i - 1] - 'a' + 1) * power[string_length - i]) % mod;
-        }
+ll calculateHashSubstring(int leftIndex, int rightIndex, vector<ll>& prefixHashes, vector<ll>& basePowers, int stringLength) {
+    if (leftIndex == 0) {
+        return (((prefixHashes[rightIndex] + 1000000007) % 1000000007) * basePowers[stringLength]) % 1000000007;
     }
-
-    long long getHash(size_t l, size_t r) const {
-        // Adjust for 1-based indexing in the original code
-        return (hashes[r] - hashes[l - 1] + mod) % mod * power[l - 1] % mod;
-    }
-
-    size_t length() const {
-        return string_length;
-    }
-};
-
-class BorderFinder {
-public:
-    static std::string findLongestBorder(const std::string& s) {
-        StringHasher hasher(s);
-        size_t n = hasher.length();
-        size_t max_border = 0;
-
-        for (size_t i = 1; i <= n - 2; ++i) {
-            long long prefix_hash = hasher.getHash(1, i);
-            long long suffix_hash = hasher.getHash(n - i + 1, n);
-
-            if (prefix_hash == suffix_hash) {
-                for (size_t j = 2; j <= n - i; ++j) {
-                    long long middle_hash = hasher.getHash(j, j + i - 1);
-                    if (middle_hash == prefix_hash) {
-                        max_border = i;
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (max_border > 0) {
-            return s.substr(0, max_border);
-        }
-        return "Just a legend";
-    }
-};
+    return (((prefixHashes[rightIndex] - prefixHashes[leftIndex - 1] + 1000000007) % 1000000007) * basePowers[stringLength - leftIndex]) % 1000000007;
+}
 
 int main() {
-    std::string input;
-    std::cin >> input;
-    std::cout << BorderFinder::findLongestBorder(input) << std::endl;
-    return 0;
+    string inputString;
+    cin >> inputString;
+
+    int stringLength = inputString.size();
+
+    vector<ll> prefixHashes(stringLength);
+    vector<ll> basePowers(stringLength + 1);
+
+    basePowers[0] = 1;
+
+    for (int i = 1; i <= stringLength; i++) {
+        basePowers[i] = basePowers[i - 1] * 27;
+        basePowers[i] %= 1000000007;
+    }
+
+    prefixHashes[0] = inputString[0] * basePowers[0];
+
+    for (int i = 1; i < stringLength; i++) {
+        prefixHashes[i] = prefixHashes[i - 1] + inputString[i] * basePowers[i];
+        prefixHashes[i] %= 1000000007;
+    }
+
+    vector<pair<int, int>> matchingLengths;
+
+    for (int i = 0; i < stringLength - 2; i++) {
+        ll startHash = calculateHashSubstring(0, i, prefixHashes, basePowers, stringLength);
+        ll endHash = calculateHashSubstring(stringLength - i - 1, stringLength - 1, prefixHashes, basePowers, stringLength);
+        if (startHash == endHash)
+            matchingLengths.push_back({ i + 1, startHash });
+    }
+
+    int leftIndex = -1;
+    int rightIndex = matchingLengths.size();
+
+    int resultLeft = -1;
+    int resultRight = stringLength;
+
+    while (rightIndex - leftIndex > 1) {
+        int midIndex = (rightIndex + leftIndex) / 2;
+        bool foundMatch = false;
+        for (int i = 1; i < stringLength - matchingLengths[midIndex].first; i++) {
+            ll midHash = calculateHashSubstring(i, i + matchingLengths[midIndex].first - 1, prefixHashes, basePowers, stringLength);
+            if (midHash == matchingLengths[midIndex].second) {
+                foundMatch = true;
+                resultLeft = i;
+                resultRight = i + matchingLengths[midIndex].first - 1;
+            }
+        }
+        if (foundMatch)
+            leftIndex = midIndex;
+        else
+            rightIndex = midIndex;
+    }
+
+    if (leftIndex == -1) {
+        cout << "Just a legend";
+    }
+    else {
+        for (int i = resultLeft; i <= resultRight; i++)
+            cout << inputString[i];
+    }
 }
